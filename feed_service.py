8c583e07e5438170de6fadd62f3ad45b2a65d12c
Ville Toiviainen <villej.toiviainen@gmail.com>
2019-11-16 21:19:30 +0200

    create mock for user

--- feed_service.py
+++ feed_service.py
@@ -3,6 +3,27 @@ from dal import feeditem_dal
 from dal import user_dal
 
 
+def create_mock_feed(user_id: str):
+    ville = user_dal.add("Ville", "Toiviainen", "Villux")["id"]
+    user_dal.update_profile_pic(ville, "/uploads/ville.jpeg")
+
+    andreas = user_dal.add("Andreas", "Urbanski", "nardeas")["id"]
+    user_dal.update_profile_pic(andreas, "/uploads/andreas.jpeg")
+
+    post1 = "What an event again! Junction is just pure awesomenes!"
+    feed_item = feeditem_dal.add(post1, user_id, ville)
+    feeditem_dal.update_image(feed_item["id"], "/uploads/junction.jpeg")
+
+    post2 = "Today I managed to catch a huge fish. I feel so lucky!"
+    feed_item = feeditem_dal.add(post2, user_id, andreas)
+
+    post3 = "Joker movie was good, but I still like Huge Ledger more. Maybe I'm just weird?"
+    feed_item = feeditem_dal.add(post3, user_id, andreas)
+
+    post4 = "Too much candy is too much. Next more wine!"
+    feed_item = feeditem_dal.add(post4, user_id, ville)
+
+
 def get_feed_and_users_with_grouped_comments(
     feed: List[Dict[Any, Any]]
 ) -> Tuple[List[Dict[Any, Any]], Set[str]]:
--- message_service.py
+++ message_service.py
@@ -3,6 +3,14 @@ from dal import message_dal
 from dal import user_dal
 
 
+def create_mock_message(user_id: str) -> None:
+    matti = user_dal.add("Matti", "Nykänen", "matti")["id"]
+    user_dal.update_profile_pic(matti, "/uploads/matti.jpeg")
+
+    message = "What's up?"
+    message_dal.add(message, user_id, matti)
+
+
 def get_messages(user_id: str) -> Dict[Any, Any]:
     messages = message_dal.get_conversation(user_id)
 
