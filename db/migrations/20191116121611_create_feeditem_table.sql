69e2b6d7f398c798881a29cfed481570b5963f0e
Ville Toiviainen <villej.toiviainen@gmail.com>
2019-11-17 00:39:39 +0200

    add refactored feed

--- db/migrations/20191116121611_create_feeditem_table.sql
+++ db/migrations/20191116121611_create_feeditem_table.sql
@@ -1,13 +1,7 @@
 -- migrate:up
-CREATE TABLE feeditem (
+CREATE TABLE feed (
             id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
             user_id UUID,
-            like_count INTEGER DEFAULT 0,
-            post_text TEXT,
-            post_image TEXT,
-            parent_id UUID,
-            creator_id UUID,
-            timestamp_ms_created BIGINT NOT NULL DEFAULT (extract(epoch from now()) * 1000),
-            timestamp_ms_updated BIGINT
+            context TEXT,
         );
 -- migrate:down
