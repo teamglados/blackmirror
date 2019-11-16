f839451c3abafd8f83b81935a929f20cdbcdfc31
Ville Toiviainen <villej.toiviainen@gmail.com>
2019-11-16 16:24:20 +0200

    upload endpoints, fixes

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify, abort, request, make_response, url_for
+import os
+from flask import Flask, jsonify, abort, request, make_response, url_for, send_from_directory
+from werkzeug.utils import secure_filename
 from webargs.flaskparser import parser
 from webargs.core import ValidationError
 from webargs.flaskparser import use_args
@@ -6,9 +8,12 @@ from webargs.flaskparser import use_args
 import error_msgs
 import validation
 from dal import user_dal
+from dal import feeditem_dal
 
+UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", "uploads")
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.errorhandler(404)
@@ -28,10 +33,45 @@ def get_user_by_id(user_id):
 
 @app.route("/api/users", methods=["POST"])
 @use_args(validation.user_args)
-def create_manifest(r_json):
-    # TODO store the image
-    image = None
+def create_user(r_json):
     user = user_dal.add(
-        r_json["first_name"], r_json["last_name"], r_json["github_user"], image
+        r_json["first_name"], r_json["last_name"], r_json["github_user"]
     )
     return jsonify(user), 201
+
+def store_image(file, user_id: str) -> str:
+    if file.filename == '':
+        raise ValueError("Filename missing!")
+    filename = secure_filename(f"{user_id}_{file.filename}")
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(filepath)
+    return filename
+
+@app.route('/uploads', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        abort(400, description=f"{error_msgs.NO_FILE_IN_REQUEST}")
+    try:
+        if request.args.get('user'):
+            user = request.args.get('userid')
+            filename = store_image(request.files['file'], user)
+            img_url_path = url_for('uploaded_file', filename=filename)
+            user_dal.update_profile_pic(user, img_url_path)
+        elif request.args.get('feed'):
+            feed = request.args.get('feedid')
+            filename = store_image(request.files['file'], feed)
+            img_url_path = url_for('uploaded_file', filename=filename)
+            feeditem_dal.update_image(feed, img_url_path)
+        return 'ok'
+    except ValueError as e:
+        abort(400, description=f"{error_msgs.NO_FILE_IN_REQUEST} - {str(e)}")
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@app.route('/ping')
+def ping():
+    return "pong"
--- error_msgs.py
+++ error_msgs.py
@@ -1,3 +1,6 @@
 RESOURCE_NOT_FOUND = "Resource not found"
 NO_ID_RETURNED = "No id returned!"
 NO_VALUE_IN_DB = "No value in database!"
+
+NO_FILE_IN_REQUEST = "File missing!"
+
