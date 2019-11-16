438829ba84b5581f7c7d2846149aba7525f6d350
Ville Toiviainen <villej.toiviainen@gmail.com>
2019-11-16 23:04:47 +0200

    remove old image store method

--- app.py
+++ app.py
@@ -64,35 +64,6 @@ def create_user(r_json):
     return jsonify(user), 200
 
 
-def store_image(file, user_id: str) -> str:
-    if file.filename == "":
-        raise ValueError("Filename missing!")
-    filename = secure_filename(f"{user_id}_{file.filename}")
-    filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-    file.save(filepath)
-    return filename
-
-
-@app.route("/uploads", methods=["POST"])
-def upload_file():
-    if "file" not in request.files:
-        abort(400, description=f"{error_msgs.NO_FILE_IN_REQUEST}")
-    try:
-        if request.args.get("user"):
-            user = request.args.get("userid")
-            filename = store_image(request.files["file"], user)
-            img_url_path = url_for("uploaded_file", filename=filename)
-            user_dal.update_profile_pic(user, img_url_path)
-        elif request.args.get("feed"):
-            feed = request.args.get("feedid")
-            filename = store_image(request.files["file"], feed)
-            img_url_path = url_for("uploaded_file", filename=filename)
-            feeditem_dal.update_image(feed, img_url_path)
-        return "ok"
-    except ValueError as e:
-        abort(400, description=f"{error_msgs.NO_FILE_IN_REQUEST} - {str(e)}")
-
-
 @app.route("/uploads/<filename>")
 def uploaded_file(filename):
     return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
