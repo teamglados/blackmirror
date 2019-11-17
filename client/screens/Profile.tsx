c9d895c8e9dca2a4738c700455bf85e4ff4e2b35
Temzasse <teemu.taskula@taitounited.fi>
2019-11-17 09:51:31 +0200

    Fine tuning

--- client/screens/Profile.tsx
+++ client/screens/Profile.tsx
@@ -10,17 +10,27 @@ import ProfileFeed from '../components/ProfileFeed';
 import Divider from '../components/common/Divider';
 import { useAppState } from '../utils/context';
 import { userDetails } from '../utils/data';
+import { WINDOW_HEIGHT } from '../constants/display';
 
 function ProfileScreen({ navigation, route }) {
   const state = useAppState();
-  console.log('> route', route.params);
   const user = get(route, 'params.user', state.user);
   const posts = get(route, 'params.posts', state.posts);
+  const [endTriggered, setEndTriggered] = React.useState(false); // prettier-ignore
+
+  function handleScroll({ nativeEvent: { contentOffset } }) {
+    if (endTriggered) return;
+
+    if (contentOffset.y > WINDOW_HEIGHT * 1.5) {
+      setEndTriggered(true);
+      navigation.navigate('End');
+    }
+  }
 
   return (
     <Wrapper>
       <ProfileHeader data={user} />
-      <Scroller>
+      <Scroller onScroll={handleScroll} scrollEventThrottle={200}>
         <ProfileDetails data={{ ...user, ...userDetails }} />
         <ProfileBio data={userDetails} />
         <ProfileFriends data={userDetails} />
