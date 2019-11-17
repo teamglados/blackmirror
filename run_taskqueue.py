8bb76eb7690bbe46c9bb98b1ec1ee9070d8224d4
Ville Toiviainen <villej.toiviainen@gmail.com>
2019-11-17 05:29:17 +0200

    set redis

--- run_taskqueue.py
+++ run_taskqueue.py
@@ -1,12 +1,13 @@
 import os
 import taskqueue
-
+import bm_redis
 RPQUEUE_PROCESSES = int(os.getenv("RPQUEUE_PROCESSES", "2"))
 RPQUEUE_THREADS_PER_PROC = int(os.getenv("RPQUEUE_THREADS_PER_PROC", "2"))
 RPQUEUE_WAIT_SHUTDOWN = int(os.getenv("RPQUEUE_WAIT_SHUTDOWN", "5"))
 
 if __name__ == "__main__":
     rpq = getattr(taskqueue, "bmqueue")
+    rpq.set_redis_connection(bm_redis.get_ephemeral())
     __import__(f"taskqueue.tasks")
     rpq.execute_tasks(
         None, RPQUEUE_THREADS_PER_PROC, RPQUEUE_PROCESSES, module="taskqueue"
